c3c71381e56d4f8692e20bd295f81274dbe2759d
Georgi Markov <markov_ood@abv.bg>
2018-10-05 15:45:36 +0300

    Added variation of first task Data Type Finder (more clever approach)

---
+++ DataTypes and Variables - More Exercises/1.1 Data Type Finder (variant)/DataTypeFinderVariant.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _1._1_Data_Type_Finder__variant_
+{
+    public class DataTypeFinderVariant
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string dataType = string.Empty;
+            while (input != "END")
+            {
+                dataType = GetDataType(input);
+                Console.WriteLine($"{input.ToLower()} is {dataType} type");
+                input = Console.ReadLine();
+            }
+        }
+
+        private static string GetDataType(string input)
+        {
+            // solve with this example
+            bool isInteger = int.TryParse(input, out int intResult);
+            bool isBoolean = bool.TryParse(input, out bool boolResult);
+            bool isChar = char.TryParse(input, out char charResult);
+            bool isFloatingPoint = double.TryParse(input, out double floatingPointResult);
+
+            if (isInteger)
+            {
+                return "integer";
+            }
+            else if (isFloatingPoint)
+            {
+                return "floating point";
+            }
+            else if (isChar)
+            {
+                return "char";
+            }
+            else if (isBoolean)
+            {
+                return "boolean";
+            }
+            else
+            {
+                return "string";
+            }
+        }
+    }
+}
