8e2d50fc46ce70dfaa9125b1127e56d4b508ee06
Georgi Markov <markov_ood@abv.bg>
2018-10-28 14:44:40 +0200

    Completed optional tasks: Anonymous Threat --> 90/100 Pokemon Don't Go --> 80/100 SoftUni Course Planning --> 100/100

---
+++ Lists - Exercise/09. Pokemon Don't Go/Pokemon_Don_t_Go.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Pokemon_Don_t_Go
+{
+    public class Pokemon_Don_t_Go
+    {
+        public static void Main()
+        {
+            List<int> distToPokemons = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> removed = new List<int>();
+            while (true)
+            {
+                if (distToPokemons.Count == 0)
+                {
+                    break;
+                }
+
+                int index = int.Parse(Console.ReadLine());
+                if (index < 0)
+                {
+                    distToPokemons[0] = distToPokemons[distToPokemons.Count - 1];
+
+                    index = 0;
+                    removed.Add(distToPokemons[index]);
+                    ProccessElements(distToPokemons[index], distToPokemons);
+                    continue;
+                }
+
+                if (index > distToPokemons.Count - 1)
+                {
+                    distToPokemons[distToPokemons.Count - 1] = distToPokemons[0];
+
+                    index = distToPokemons.Count - 1;
+                    removed.Add(distToPokemons[index]);
+                    ProccessElements(distToPokemons[index], distToPokemons);
+                    continue;
+                }
+
+                int elementToRemove = distToPokemons[index];
+                distToPokemons.RemoveAt(index);
+                removed.Add(elementToRemove);
+                ProccessElements(elementToRemove, distToPokemons);
+            }
+
+            checked
+            {
+                int sum = removed.Sum();
+                Console.WriteLine(sum);
+            }
+        }
+
+        private static void ProccessElements(int elementToRemove, List<int> distToPokemons)
+        {
+            for (int i = 0; i < distToPokemons.Count; i++)
+            {
+                int currentElement = distToPokemons[i];
+                if (currentElement <= elementToRemove)
+                {
+                    distToPokemons[i] += elementToRemove;
+                }
+                else
+                {
+                    distToPokemons[i] -= elementToRemove;
+                }
+            }
+        }
+    }
+}
---
+++ Lists - Exercise/10. SoftUni Course Planning/SoftUni_Course_Planning.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._SoftUni_Course_Planning
+{
+    public class SoftUni_Course_Planning
+    {
+        public static void Main()
+        {
+            List<string> lessonsAndExercises = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x)
+                .ToList();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "course start")
+                {
+                    break;
+                }
+
+                string[] commandDetails = command.Split(':');
+                switch (commandDetails[0])
+                {
+                    case "Add":
+                        string lessonTitle = commandDetails[1];
+                        if (!lessonsAndExercises.Contains(lessonTitle))
+                        {
+                            lessonsAndExercises.Add(lessonTitle);
+                        }
+                        break;
+                    case "Insert":
+                        lessonTitle = commandDetails[1];
+                        int index = int.Parse(commandDetails[2]);
+                        if (!lessonsAndExercises.Contains(lessonTitle))
+                        {
+                            lessonsAndExercises.Insert(index, lessonTitle);
+                        }
+                        break;
+                    case "Remove":
+                        lessonTitle = commandDetails[1];
+                        if (lessonsAndExercises.Contains(lessonTitle))
+                        {
+                            lessonsAndExercises.Remove(lessonTitle);
+                            string exerciseName = $"{lessonTitle}-Exercise";
+                            int indexOfExerciseName = lessonsAndExercises.IndexOf(exerciseName);
+                            if (indexOfExerciseName >= 0)
+                            {
+                                lessonsAndExercises.RemoveAt(indexOfExerciseName);
+                            }
+                        }
+                        break;
+                    case "Swap":
+                        lessonTitle = commandDetails[1];
+                        string otherLessonTitle = commandDetails[2];
+                        if (lessonsAndExercises.Contains(lessonTitle) &&
+                            lessonsAndExercises.Contains(otherLessonTitle))
+                        {
+                            Swap(lessonsAndExercises, lessonTitle, otherLessonTitle);
+                            string firstLessonExerciseName = $"{lessonTitle}-Exercise";
+                            string secondLessonExerciseName = $"{otherLessonTitle}-Exercise";
+                            if (lessonsAndExercises.Contains(firstLessonExerciseName))
+                            {
+                                // put it right behind the lesson
+                                int indexOfLesson = lessonsAndExercises.IndexOf(lessonTitle);
+                                int indexOfExercise = lessonsAndExercises.IndexOf(firstLessonExerciseName);
+                                lessonsAndExercises.RemoveAt(indexOfExercise);
+                                lessonsAndExercises.Insert(indexOfLesson + 1, firstLessonExerciseName);
+                            }
+
+                            if (lessonsAndExercises.Contains(secondLessonExerciseName))
+                            {
+                                // put it right behind the lesson
+                                int indexOfLesson = lessonsAndExercises.IndexOf(otherLessonTitle);
+                                int indexOfExercise = lessonsAndExercises.IndexOf(secondLessonExerciseName);
+                                lessonsAndExercises.RemoveAt(indexOfExercise);
+                                lessonsAndExercises.Insert(indexOfLesson + 1, secondLessonExerciseName);
+                            }
+                        }
+                        break;
+                    case "Exercise":
+                        lessonTitle = commandDetails[1];
+                        bool lessonExists = lessonsAndExercises.Contains(lessonTitle);
+                        string exerciseTitle = $"{lessonTitle}-Exercise";
+                        bool exerciseExists = lessonsAndExercises.Contains(exerciseTitle);
+                        if (lessonExists && !exerciseExists)
+                        {
+                            int indexOfLesson = lessonsAndExercises.IndexOf(lessonTitle);
+                            lessonsAndExercises.Insert(indexOfLesson + 1, exerciseTitle);
+                        }
+                        else if (!lessonExists)
+                        {
+                            lessonsAndExercises.Add(lessonTitle);
+                            lessonsAndExercises.Add(exerciseTitle);
+                        }
+                        break;
+                }
+
+            }
+
+            int lessonIndex = 1;
+            foreach (var item in lessonsAndExercises)
+            {
+                Console.WriteLine($"{lessonIndex}.{item}");
+                lessonIndex++;
+            }
+        }
+
+        private static void Swap(List<string> courseProgramma, string firstLessonTitle, string secondLessonTitle)
+        {
+            int firstLessonIndex = courseProgramma.IndexOf(firstLessonTitle);
+            int secondLessonIndex = courseProgramma.IndexOf(secondLessonTitle);
+
+            string temp = courseProgramma[firstLessonIndex];
+            courseProgramma[firstLessonIndex] = courseProgramma[secondLessonIndex];
+            courseProgramma[secondLessonIndex] = temp;
+        }
+    }
+}
