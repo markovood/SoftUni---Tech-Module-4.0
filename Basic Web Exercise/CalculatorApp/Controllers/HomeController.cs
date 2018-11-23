75c1f34a2ca4b61b64e55bf2b0f9476e4c51e13e
Georgi Markov <markov_ood@abv.bg>
2018-11-23 23:00:56 +0200

    Completed Basic Web Exercise

---
+++ Basic Web Exercise/CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculator calculator)
+        {
+            return View(calculator); // works without passing calculator WHY???
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(Calculator calculator)
+        {
+            if (calculator.Operator == "/" && calculator.RightOperand == 0m)
+            {
+                TempData["ErrorMsg"] = "Dividing by 0 is not allowed!";
+            }
+            else
+            {
+                calculator.CalculateResult();
+                DataStorage.Calculations.Add(calculator);
+            }
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        public IActionResult History()
+        {
+            return View(DataStorage.Calculations);
+        }
+
+    }
+}
---
+++ Basic Web Exercise/CalculatorApp/Models/DataStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Models
+{
+    public static class DataStorage
+    {
+        public static List<Calculator> Calculations = new List<Calculator>();
+    }
+}
