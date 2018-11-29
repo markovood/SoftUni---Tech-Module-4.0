351ab91a0e2d12181d03bca22adda491c1e5b626
Georgi Markov <markov_ood@abv.bg>
2018-11-29 23:03:08 +0200

    initial commit

---
+++ Basic CRUD Exercise/TeisterMask/Data/TeisterMaskDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace TeisterMask.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using TeisterMask.Models;
+
+    public class TeisterMaskDbContext : DbContext
+    {
+        private const string CONNECTION_STRING = @"Server=.\SQLEXPRESS;Database=TeisterMaskDb;Integrated Security=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(CONNECTION_STRING);
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
